b9b65bbb7502caa6c98524fbc931ae18766b506c
alnur05 <alnurt00@gmail.com>
2023-05-31 17:50:23 +0600

    send coins list

--- go.mod
+++ go.mod
@@ -3,13 +3,6 @@ module mod
 go 1.19
 
 require (
-	github.com/adshao/go-binance/v2 v2.4.2 // indirect
-	github.com/bitly/go-simplejson v0.5.0 // indirect
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	_ "github.com/lib/pq"
@@ -15,6 +16,11 @@ import (
 type bnResp struct {
 	Price float64 `json:"price,string,omitempty"`
 	Code  int64   `json:"code"`
+}
+
+type coin struct {
+	id     int
+	crypto string
 }
 
 func main() {
@@ -31,28 +37,65 @@ func main() {
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
-	connStr := "user=postgres password=adletloh695 dbname=db sslmode=disable"
-
+	connStr := "user=postgres password= dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	defer db.Close()
 	for update := range updates {
 		if update.Message == nil { // If we got a message
 			continue
 		}
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		price, _ := getPrice(update.Message.Text)
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
-		}
-		defer db.Close()
-		result, err := db.Exec("insert into crytpocoin (crypto) values ($1)", update.Message.Text)
-		if err != nil {
-			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
-		}
-		fmt.Println(result.LastInsertId()) // не поддерживается
-		fmt.Println(result.RowsAffected()) // количество добавленных строк
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%f", price))
+		command := strings.Split(update.Message.Text, " ")
+		switch command[0] {
+		case "ADD":
+			if len(command) != 2 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
+			}
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			result, err := db.Exec("insert into cryptocoin (crypto) values ($1)", command[1])
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+			}
+			fmt.Println(result.LastInsertId())
+			fmt.Println(result.RowsAffected())
 
-		bot.Send(msg)
+		case "DEL":
+			if len(command) != 2 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
+			}
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			result, err := db.Exec("delete from cryptocoin where crypto = $1", command[1])
+			fmt.Println(result.RowsAffected()) // количество удаленных строк
+			if err != nil {
+				panic(err)
+			}
+		case "START":
+			msg := ""
+			if len(command) != 1 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверная команда"))
+			}
+			crypto, err := db.Query("select * from cryptocoin")
+			if err != nil {
+				panic(err)
+			}
+			coins := []coin{}
+			for crypto.Next() {
+				p := coin{}
+				err := crypto.Scan(&p.id, &p.crypto)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				fmt.Println(err)
+				coins = append(coins, p)
+			}
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			for _, p := range coins {
+				price, _ := getPrice(p.crypto)
+				msg += fmt.Sprintf("%s: %f\n", p.crypto, price)
+			}
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
+		}
 	}
 }
 
@@ -69,7 +112,7 @@ func getPrice(symbol string) (price float64, err error) {
 		return
 	}
 	if jsonResp.Code != 0 {
-		err = errors.New("Неверный символ")
+		err = errors.New("�������� ������")
 	}
 	price = jsonResp.Price
 
